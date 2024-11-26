2e252c5181dcc3ba3d6c4e1102f8a44f613d759b
Hiruna Pramuditha <hiruna.nishan@gmail.com>
2024-11-26 13:54:32 +0530

    DeliveryCommentview file added

---
+++ app/models/DeliveryCommentView.php
@@ -0,0 +1,55 @@
+<?php
+
+class DeliveryCommentView
+{
+    use Model;
+
+    protected $table = 'comment';
+    protected $allowed = ['CommentID', 'OrderID', 'DeliveryID', 'driverId'];
+
+    public function getComments($deliveryID)
+    {
+        $data = ['DeliveryID' => $deliveryID];
+        $this->setLimit(null);
+        $this->setOffset(null);
+
+        return $this->where($data);
+    }
+
+    public function updateComments($commentID, $comment)
+    {
+        if (!is_numeric($commentID)) {
+            $this->errors['error'] = 'Invalid Comment ID';
+            return false;
+        }
+
+        $data = [
+            'comment' => $comment
+        ];
+
+        return $this->update($commentID, $data, 'CommentID');
+    }
+
+    public function createComment($DriverID, $DeliveryID, $comment)
+    {
+        $data = [
+            'DeliveryID' => $DeliveryID,
+            'DriverID' => $DriverID,
+            'comment' => $comment
+        ];
+
+        return $this->insert($data);
+    }
+
+    public function deleteComment($commentID)
+    {
+        return $this->delete($commentID, 'CommentID');
+    }
+
+    public function getOneComment($commentID)
+    {
+        $data = ["commentId" => $commentID];
+
+        return $this->first($data);
+    }
+}

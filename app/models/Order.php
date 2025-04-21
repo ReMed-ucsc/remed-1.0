edf886e5c0b5be1d792a184250842773c9224622
hiruna <hiruna.nishan@gmail.com>
2025-04-21 14:42:34 +0530

    order confim added

--- app/models/Order.php
+++ app/models/Order.php
@@ -1,10 +1,12 @@
 <?php
 
+require_once BASE_PATH . '/api/coontrollers/utilis/DeliveryUtility.php';
+
 class Order
 {
     use Model;
 
-    protected $table = 'medicineorder';
+    protected $table = 'medicineOrder';
     protected $allowedColumns = ['OrderID', 'PatientID', 'Delivery Address', 'Date', 'Payment'];
     protected $order_column = "ProductID";
 
@@ -17,5 +19,31 @@ class Order
     public function getMedicineOrder($orderID)
     {
         return $this->first(['OrderId' => $orderID]);
+    }
+
+    public function confirmOrder($orderId)
+    {
+        $orderModel = new MedicineOrder();
+        $utilityModel = new DeliveryUility();
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $orderId = $_POST['orderId'];
+            $status = 'W';
+
+            $status = $orderModel->getStatusName('Q');
+
+            $orderModel->updateOrderStatus($orderId, $status);
+
+            //add the logic to only execute if the order is to be delivered
+            //check if the order is store pickup
+            $utilityModel->sendDetailstoDriver($orderId);
+
+            //redirect to all orderes viewwing page 
+            //change to appropiate redirection
+            redirect("orderMain");
+            exit;
+        }
+
+        //redirect("orderMain");
     }
 }

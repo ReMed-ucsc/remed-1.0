9be9d93c465159a6f5e46e3c98fa8f113fbcd753
lakshan102 <lakshan2725@gmail.com>
2025-04-23 02:43:18 +0530

    i had some error in input value part now resolve it

--- app/views/admin/accountManage.view.php
+++ app/views/admin/accountManage.view.php
@@ -1,37 +1,49 @@
 <?php
-require_once BASE_PATH.'/app/views/inc/header.view.php';
-require_once BASE_PATH.'/app/views/inc/navBar.view.php';
+require_once BASE_PATH . '/app/views/inc/header.view.php';
+require_once BASE_PATH . '/app/views/inc/navBar.view.php';
 ?>
 
 <body>
-<h2 class="page-title">Admin Account Management</h2>
+    <h2 class="page-title">Admin Account Management</h2>
 
-<div class="details-container">
-    <form class="Form" action="" method="POST">
-        <div>
-            <label for="name">Name:</label>
-            <input class="Input" type="text" id="name" name="username" placeholder="Enter name" value="<?= htmlspecialchars($admin->username) ?>" required>
-        </div>
+    <div class="details-container">
+        <form class="Form" action="" method="POST">
+            
 
-        <div>
-            <label for="email">Email:</label>
-            <input class="Input" type="email" id="email" name="email" placeholder="Enter email" value="<?= htmlspecialchars($admin->email) ?>" required>
-        </div>
+            <div>
+                <label for="name">Name:</label>
+                <input class="Input" type="text" id="name" name="username" placeholder="Enter name"
+                    value="<?= isset($data['username']) ? htmlspecialchars($data['username']) : '' ?>" required>
+            </div>
 
-        <div>
-            <label for="password">Password:</label>
-            <input class="Input" type="password" id="password" name="password" placeholder="Enter password" value="<?= htmlspecialchars($admin->password) ?>" required>
-        </div>
+            <div>
+                <label for="email">Email:</label>
+                <input class="Input" type="email" id="email" name="email" placeholder="Enter email"
+                    value="<?= isset($data['email']) ? htmlspecialchars($data['email']) : '' ?>" required>
+            </div>
 
-        <div>
-            <label for="confirm_password">Confirm password:</label>
-            <input class="Input" type="password" id="confirm_password" name="confirm_password" placeholder="Re-enter password" required>
-        </div>
+            <div>
+                <label for="contactNo">ContactNo:</label>
+                <input class="Input" type="tel" id="contactNo" name="contactNo" placeholder="Enter Contact No"
+                    value="<?= isset($data['contactNo']) ? htmlspecialchars($data['contactNo']) : '' ?>" required>
+            </div>
 
-        <div>
-            <button type="submit" class="btn-green">Save changes</button>
-        </div>
-    </form>
-</div>
+            <div>
+                <label for="password">Password:</label>
+                <input class="Input" type="password" id="password" name="password" placeholder="Enter password"
+                    value="<?= isset($data['password']) ? htmlspecialchars($data['password']) : '' ?>" required>
+            </div>
 
-<?php require_once BASE_PATH.'/app/views/inc/footer.view.php'; ?>
+            <div>
+                <label for="confirm_password">Confirm password:</label>
+                <input class="Input" type="password" id="confirm_password" name="confirm_password"
+                    placeholder="Re-enter password" required>
+            </div>
+
+            <div>
+                <button type="submit" class="btn-green">Save changes</button>
+            </div>
+        </form>
+    </div>
+
+    <?php require_once BASE_PATH . '/app/views/inc/footer.view.php'; ?>

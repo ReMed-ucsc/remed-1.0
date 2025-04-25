a20033e6df92bfbf6f030a0041f0e094d211e13e
hiruna <hiruna.nishan@gmail.com>
2025-04-26 01:31:03 +0530

    delivery related files modefied

--- api/controllers/delivery/ConfirmDeliveryController.php
+++ api/controllers/delivery/ConfirmDeliveryController.php
@@ -6,17 +6,17 @@ require_once BASE_PATH . '/app/models/Delivery.php';
 require_once BASE_PATH . '/app/models/Order.php';
 require_once BASE_PATH . '/app/models/MedicineOrder.php';
 require_once BASE_PATH . '/app/models/Driver.php';
+require_once BASE_PATH . '/app/models/Notification.php';
 
 class ConfirmDeliveryController
 {
     public function index()
     {
-        $response = array();
+        $response = [];
         $result = new Result();
 
         $input = file_get_contents("php://input");
         $data = json_decode($input, true);
-
         if (
             empty($data['orderId']) ||
             empty($data['auth_token']) ||
@@ -24,46 +24,45 @@ class ConfirmDeliveryController
             empty($data['driverId'])
         ) {
             http_response_code(400);
-
             $result->setErrorStatus(true);
             $result->setMessage("All fileds are required");
         } else {
             $driverModel = new Driver();
             $tokenStatus = $driverModel->verifyToken($data['driverId'], $data['auth_token']);
-
             if (!$tokenStatus) {
+                http_response_code(400);
                 $result->setErrorStatus(true);
                 $result->setMessage("Invalid token");
             } else {
                 $delivery = new Delivery();
                 $order = new Order();
-
                 $orderId = $data['orderId'];
                 $deliveryId = $data['deliveryId'];
-
                 $orderResult = $order->getMedicineOrder($orderId);
-
                 if (!$orderResult) {
+                    http_response_code(404);
                     $result->setErrorStatus(true);
                     $result->setMessage("No order found");
                 } else {
                     $deliveryResult = $delivery->getDeliveryInfo($deliveryId);
-
                     if (!$deliveryResult) {
+                        http_response_code(404);
                         $result->setErrorStatus(true);
                         $result->setMessage("No delivery found");
                     } else {
                         $delivery->changeDeliveryStatus($deliveryId, "Delivered");
-
+                        $delivery->setDistance($deliveryId, $data['totalDistance']);
                         $result->setErrorStatus(false);
                         $result->setMessage("Updated successfully");
+
+                        $notificationModel = new Notification();
+                        $notificationModel->createNotification($orderResult->PharmacyID, $orderId, "Order # $orderId delivered");
                     }
                 }
             }
         }
-
-        $response['error'] = $result->isError();
-        $response['message'] = $result->getMessage();
+        $response['result']['error'] = $result->isError();
+        $response['result']['message'] = $result->getMessage();
 
         echo json_encode($response);
     }

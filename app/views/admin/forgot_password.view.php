1dc9271d1efa5ec96a40236e0e1d15addc7d0f1e
lakshan102 <lakshan2725@gmail.com>
2025-04-25 18:37:43 +0530

    css added

--- app/views/admin/forgot_password.view.php
+++ app/views/admin/forgot_password.view.php
@@ -1,8 +1,28 @@
-<h2>Forgot Password</h2>
+<?php
 
-<form method="post" action="<?= ROOT ?>/admin/AuthController/sendResetLink">
-  <label for="email">Enter your email address:</label>
-  <input type="email" name="email" id="email" required>
+require_once BASE_PATH . '/app/views/inc/header.view.php'
 
-  <button type="submit">Send Reset Link</button>
-</form>
+  ?>
+
+<body class="login-body">
+  <div class="login-container-out">
+    <div class="login-container">
+      <div class="login-left">
+        <img src="<?= ROOT ?>/assets/images/ReMeD.png" alt="logo">
+        <p>ONLINE PHARMACY LOCATOR <br> AND <br> MEDICINE TRACKER</p>
+        <h3>Forgot Password</h3>
+      </div>
+      <div class="login-right">
+        <form method="post" action="<?= ROOT ?>/admin/AuthController/sendResetLink">
+          <label for="email">Enter your email address:</label>
+          <input type="email" name="email" id="email" required>
+
+          <button type="submit">Send Reset Link</button>
+        </form>
+      </div>
+    </div>
+
+
+  </div>
+</body>
+<?php require_once BASE_PATH . '/app/views/inc/footer.view.php' ?>
--- app/views/admin/reset_password.view.php
+++ app/views/admin/reset_password.view.php
@@ -1,8 +1,26 @@
-<h2>Reset Your Password</h2>
+<?php
+require_once BASE_PATH . '/app/views/inc/header.view.php'
+?>
 
-<form method="post" action="<?= ROOT ?>/admin/AuthController/resetPassword">
-  <input type="hidden" name="token" value="<?= htmlspecialchars($token) ?>">
-  <label>New Password:</label>
-  <input type="password" name="password" required>
-  <button type="submit">Reset Password</button>
-</form>
+<body class="login-body">
+  <div class="login-container-out">
+    <div class="login-container">
+      <div class="login-left">
+        <img src="<?= ROOT ?>/assets/images/ReMeD.png" alt="logo">
+        <p>ONLINE PHARMACY LOCATOR <br> AND <br> MEDICINE TRACKER</p>
+        <h3>Reset Your Password</h3>
+      </div>
+      <div class="login-right">
+        <form method="post" action="<?= ROOT ?>/admin/AuthController/resetPassword">
+          <input type="hidden" name="token" value="<?= htmlspecialchars($token) ?>">
+          <label>New Password:</label>
+          <input type="password" name="password" required>
+          <button type="submit">Reset Password</button>
+        </form>
+      </div>
+    </div>
+
+
+  </div>
+</body>
+<?php require_once BASE_PATH . '/app/views/inc/footer.view.php' ?>

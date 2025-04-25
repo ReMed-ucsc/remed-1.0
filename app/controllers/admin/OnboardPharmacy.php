f570f2199512dd687d37ba0512cdb53f3b1325ab
lakshan102 <lakshan2725@gmail.com>
2025-04-25 10:02:24 +0530

    onboard pharmacy page done

---
+++ app/controllers/admin/OnboardPharmacy.php
@@ -0,0 +1,20 @@
+<?php
+
+class OnboardPharmacy
+{
+    use Controller;
+    public function index()
+    {
+        $msg = new Pharmacy();
+        $driver = new Driver();
+
+        $MsgDriver = $driver->notificationDriver('pending');
+        $Msg = $msg->notification('pending');
+
+        $data=[
+            'notification'=>$Msg,
+            'notificationDriver'=>$MsgDriver
+        ];
+        $this->view('admin/onboardPharmacy', $data);
+    }
+}

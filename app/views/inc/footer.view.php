2b2126c0761ca5a717b28886d0d7bd5e3f660fc7
hiruna <hiruna.nishan@gmail.com>
2025-04-28 09:33:36 +0530

    new footer

--- app/views/inc/footer.view.php
+++ app/views/inc/footer.view.php
@@ -1,17 +1,3 @@
 </body>
 
-<form method="GET" id="dateForm">
-    <div class="search-bar-styles">
-        <label for="date">Select Date:</label>
-        <input type="date" name="date" id="date" class="select" value="<?= htmlspecialchars($date ?? date('Y-m-d')) ?>">
-    </div>
-</form>
-
-<script>
-    document.getElementById('date').addEventListener('change', function() {
-        document.getElementById('dateForm').submit();
-    });
-</script>
-
-
 </html>
